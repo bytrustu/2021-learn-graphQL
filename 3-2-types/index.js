aae83a751724062bfa3a742c9f44b3bce5bc980b
bytrustu <bytrustu@gmail.com>
2021-01-22 18:45:54 +0900

    3-2 types

--- 3-2-types/index.js
+++ 3-2-types/index.js
@@ -4,10 +4,12 @@ const queries = require('./typedefs-resolvers/_queries')
 const mutations = require('./typedefs-resolvers/_mutations')
 const equipments = require('./typedefs-resolvers/equipments')
 const supplies = require('./typedefs-resolvers/supplies')
+const enums = require('./typedefs-resolvers/_enums')
 
 const typeDefs = [
     queries,
     mutations,
+    enums,
     equipments.typeDefs,
     supplies.typeDefs
 ]
